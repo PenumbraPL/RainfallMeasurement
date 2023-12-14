229ff4b908af98bc7efbbaccbb29d12ca0e1fc49
Jan Bizon <janek.bizon@gmail.com>
2023-12-14 15:35:38 +0100

    Change to data type collected from arduino.

--- api.py
+++ api.py
@@ -37,7 +37,14 @@ def read(window, go_event):
                 print(data[1])
             flowRate = flowRate if flowRate else savedData
             savedData = flowRate
-            window.write_event_value("Working", (flowRate))
+            pumpState = 'Unknown'
+            waterLevel = '13 mm'
+            rainfall = '10 mm'
+
+            data = { 'water' : str(flowRate),
+                'pump' : pumpState, 'level': waterLevel, 'rainfall' : rainfall}
+
+            window.write_event_value("Working", (data))
         except Exception as ex:
             print("Data is not readable!")
             print(ex)
@@ -46,18 +53,24 @@ def read(window, go_event):
 def gui_init():
     layout = [[sg.Text('Podaj wysokość pojemnika w mm', key="height"), sg.InputText(key="in1")],
               [sg.Text('Podaj objętość pojemnika', key="vol"), sg.InputText(key="in2")],
-              [sg.Text("", key="label", visible=False)],
+              [sg.Text("", key="water", visible=False)],
+              [sg.Text("", key="rainfall", visible=False)],
+              [sg.Text("", key="level", visible=False)],
+              [sg.Text("", key="pump", visible=False)],
               [sg.Button('Zatwierdz', key="Start")]]
 
-    window = sg.Window("Pomiar opadów", layout,size=(300, 150))
+    window = sg.Window("Pomiar opadów", layout,size=(300, 250))
 
     return window
 
 
-def gui_update(window, w, o, p):
-    window["height"].Update("Wysokość pojemnika: " + w)
-    window["vol"].Update("Objętość pojemnika: " + o)
-    window["label"].Update("Odczyt z arduino: " + p, visible=True)
+def gui_update(window, data):
+    window["height"].Update("Wysokość pojemnika: \t" + data["height"])
+    window["vol"].Update("Objętość pojemnika: \t" + data["vol"])
+    window["water"].Update("Ilość wody: \t\t" + data["water"], visible=True)
+    window["pump"].Update("Pompa włączona: \t\t1" + data["pump"], visible=True)
+    window["level"].Update("Wysokość wody: \t\t1" + data["level"], visible=True)
+    window["rainfall"].Update("Ilość opadów: \t\t1" + data["rainfall"], visible=True)
     window["in1"].Update(visible=False)
     window["in2"].Update(visible=False)
     window["Start"].Update(visible=False)
@@ -87,12 +100,12 @@ if __name__ == "__main__":
                 window["label"].Update("Wysokość i objętość muszą być liczbami!", visible=True)
                 print("Wysokość i objętość muszą być liczbami")
         elif event == "Working":
-            height_left = values[event]
-            val1 = values['in1']
-            val2 = values['in2']
-            curr_height_in_per = str((int(val1)-int(height_left))/int(val1))
+            data = values[event]
+            #str((int(data["height"])-int(data["water"]))/int(data["height"]))
+            data.update({'height' : values['in1'], 'vol' : values['in2']})
+
             try:
-                gui_update(window, values['in1'], values['in2'], curr_height_in_per)
+                gui_update(window, data)
             except Exception as ex:
                 print("sth went wrong - check data")
                 print(ex)
